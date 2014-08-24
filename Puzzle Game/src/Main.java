0c2f34f65efea1f2deeee6c7a28f53f5c8857899
Shane <Shane@Shane-PC.home>
2014-08-24 19:10:54 +0000

    readme

--- Puzzle Game/src/Main.java
+++ Puzzle Game/src/Main.java
@@ -1,10 +1,11 @@
+import ga.shane.puzzlegame.PuzzleSelector;
 import ga.shane.puzzlegame.RandomPuzzleGetter;
-
 import java.io.File;
 
 /** @author http://www.shane.ga */
 public class Main {
 	public static void main(String[] args) { 
+		//new PuzzleSelector(4, new File("images/tiger.jpg"));
 		new RandomPuzzleGetter(4, new File("images"));
 	}
 }
